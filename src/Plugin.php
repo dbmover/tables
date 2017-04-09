2ec5bd803b3e11370b6af83e88ba56e05ab98b35
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-09 11:27:07 +0200

    sort-of let tables be migrated again, but i am not REALLY happy yet with how this works

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,8 +12,24 @@ use PDO;
 
 class Plugin extends Core\Plugin
 {
+    private $columns;
+
     public function __invoke(string $sql) : string
     {
+        $type = 'column_type';
+        switch ($this->loader->getVendor()) {
+            case 'pgsql': $type = 'data_type column_type'; break;
+        }
+        $this->columns = $this->loader->getPdo()->prepare(
+            "SELECT
+                column_name,
+                column_default,
+                is_nullable,
+                $type
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE ((TABLE_CATALOG = ? AND TABLE_SCHEMA = 'public') OR TABLE_SCHEMA = ?)
+                AND TABLE_NAME = ?
+            ORDER BY ORDINAL_POSITION ASC");
         $tables = [];
         $exists = $this->loader->getPdo()->prepare(
             "SELECT * FROM INFORMATION_SCHEMA.TABLES
@@ -28,7 +44,8 @@ class Plugin extends Core\Plugin
                 if (false === $exists->fetch(PDO::FETCH_ASSOC)) {
                     $this->loader->addOperation($table[0]);
                 } else {
-                    // Check if the table needs modifications.
+                    // ...else check if the table needs modifications.
+                    $this->checkTableStatus($table[1], $table[2]);
                 }
                 $sql = str_replace($table[0], '', $sql);
             }
@@ -46,6 +63,74 @@ class Plugin extends Core\Plugin
             }
         }
         return $sql;
+    }
+
+    /**
+     * Compare the table status against the requested SQL, and generate ALTER
+     * statements accordingly.
+     *
+     * @param string $table Name of the table
+     * @param string $sql SQL of the requested table definition
+     */
+    protected function checkTableStatus(string $table, string $sql)
+    {
+        $sql = preg_replace("@^\s+@ms", '', $sql);
+        $requestedColumns = [];
+        foreach (preg_split("@,\n@m", $sql) as $reqCol) {
+            preg_match("@^[^\s]+@", $reqCol, $name);
+            $name = $name[0];
+            $reqCol = str_replace('PRIMARY KEY', '', $reqCol);
+            $requestedColumns[$name] = [
+                'column_name' => $name,
+                'column_default' => null,
+                'is_nullable' => true,
+                'column_type' => '',
+                '_definition' => trim($reqCol),
+            ];
+            $reqCol = preg_replace("@^$name\s+@", '', $reqCol);
+            if (strpos($reqCol, 'NOT NULL')) {
+                $requestedColumns[$name]['is_nullable'] = false;
+                $reqCol = str_replace('NOT NULL', '', $reqCol);
+            }
+            if (preg_match("@DEFAULT\s+(.*?)$@", $reqCol, $default)) {
+                $parsed = preg_replace("@(^'|'$)@", '', $default[1]);
+                $requestedColumns[$name]['column_default'] = $parsed;
+                $requestedColumns[$name]['_default'] = $default[1];
+                $reqCol = str_replace($default[0], '', $reqCol);
+            }
+            $requestedColumns[$name]['column_type'] = trim($reqCol);
+        }
+        $this->columns->execute([$this->loader->getDatabase(), $this->loader->getDatabase(), $table]);
+        $currentColumns = [];
+        foreach ($this->columns->fetchAll(PDO::FETCH_ASSOC) as $column) {
+            if (!isset($requestedColumns[$column['column_name']])) {
+                $this->loader->addOperation("ALTER TABLE $table DROP COLUMN {$column['column_name']};");
+            } else {
+                $column['is_nullable'] = $column['is_nullable'] == 'YES';
+                $column['column_type'] = strtoupper($column['column_type']);
+                $currentColumns[$column['column_name']] = $column;
+            }
+        }
+        foreach ($requestedColumns as $name => $col) {
+            if (!isset($currentColumns[$name])) {
+                $this->loader->addOperation("ALTER TABLE $table ADD COLUMN {$col['_definition']};");
+                continue;
+            }
+            if ($this->loader->getVendor() == 'mysql') {
+                $this->loader->addOperation("ALTER TABLE $table CHANGE COLUMN $name {$col['_definition']};");
+            } else {
+                if (strlen($col['column_default'])) {
+                    $this->loader->addOperation("ALTER TABLE $table ALTER COLUMN $name SET DEFAULT {$col['_default']};");
+                } else {
+                    $this->loader->addOperation("ALTER TABLE $table ALTER COLUMN $name DROP DEFAULT;");
+                }
+                if ($col['is_nullable']) {
+                    $this->loader->addOperation("ALTER TABLE $table ALTER COLUMN $name DROP NOT NULL");
+                } else {
+                    $this->loader->addOperation("ALTER TABLE $table ALTER COLUMN $name SET NOT NULL");
+                }
+            }
+        }
     }
 
     /*
